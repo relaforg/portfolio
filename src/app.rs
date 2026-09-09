d10f9b0303be9d99f0477308958dbca704da61a5
relaforg <laforgueremi@gmail.com>
2026-09-09 15:44:20 +0200

    Header

---
+++ src/app.rs
@@ -0,0 +1,54 @@
+use leptos::prelude::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    view! {
+        <div class="mx-auto max-w-250 px-4">
+            <Nav />
+        </div>
+    }
+}
+
+#[component]
+fn Nav() -> impl IntoView {
+    view! {
+        <header class="py-6 flex justify-between items-center">
+            <p>relaforg<span class="text-accent-500">@</span>dev</p>
+            <div class="flex items-center gap-5">
+                <nav class="flex items-center gap-5">
+                    <a href="#">"projects"</a>
+                    <a href="#">"repos"</a>
+                    <a href="#">"contact"</a>
+                </nav>
+                <button class="py-1 px-3 border border-line rounded-sm cursor-pointer">
+                    "FR / en"
+                </button>
+                <button class="cursor-pointer" aria-label="Change theme">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        class="size-4"
+                        aria-hidden="true"
+                    >
+                        <circle cx="12" cy="12" r="4" />
+                        <path d="M12 2v2" />
+                        <path d="M12 20v2" />
+                        <path d="m4.93 4.93 1.41 1.41" />
+                        <path d="m17.66 17.66 1.41 1.41" />
+                        <path d="M2 12h2" />
+                        <path d="M20 12h2" />
+                        <path d="m6.34 17.66-1.41 1.41" />
+                        <path d="m19.07 4.93-1.41 1.41" />
+                    </svg>
+                </button>
+            </div>
+        </header>
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use leptos::prelude::*;
+mod app;
+
+use app::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p class="bg-blue-500">"Hello, world!"</p> })
+    leptos::mount::mount_to_body(App)
 }
