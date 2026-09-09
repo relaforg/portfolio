ef4ec3906cab2e8292ed296564bf7c5d1840e14e
relaforg <laforgueremi@gmail.com>
2026-09-09 14:02:57 +0200

    setup tailwind

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use leptos::prelude::*;
 
 fn main() {
     console_error_panic_hook::set_once();
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    leptos::mount::mount_to_body(|| view! { <p class="bg-blue-500">"Hello, world!"</p> })
 }
